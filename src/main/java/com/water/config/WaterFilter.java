6d4aeae71a7fb069472b2288f3b310340cd52138
gaojian <1106264855@qq.com>
2016-06-07 11:58:10 +0800

    //2016年6月7日11:57:56

---
+++ src/main/java/com/water/config/WaterFilter.java
@@ -0,0 +1,67 @@
+package com.water.config;
+
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.jfinal.plugin.c3p0.C3p0Plugin;
+import com.jfinal.plugin.redis.RedisPlugin;
+import com.jfinal.render.ViewType;
+
+public class WaterFilter extends JFinalConfig {
+
+ /**
+  * 配置常量
+  */
+ @Override
+ public void configConstant(Constants me) {
+  loadPropertyFile("Config.properties");
+  // TODO Auto-generated method stub
+  me.setDevMode(true); // 设置是否是开发模式
+  me.setViewType(ViewType.VELOCITY); // 设置试图
+ }
+
+ /**
+  * 配置处理器
+  */
+ @Override
+ public void configHandler(Handlers ha) {
+  // 前期可不配。后期完善
+ }
+
+ /**
+  * 配置拦截器
+  */
+ @Override
+ public void configInterceptor(Interceptors arg0) {
+  // 前期可不配。后期完善
+
+ }
+
+ /**
+  * 配置插件
+  */
+ @Override
+ public void configPlugin(Plugins me) {
+  // 简单配置redis
+  RedisPlugin redisplugin = new RedisPlugin(this.getProperty("redisName"), this.getProperty("redisHost"));
+  me.add(redisplugin);
+  C3p0Plugin c3p0Plugin = new C3p0Plugin(this.getProperty("jdbcurl"), this.getProperty("user"), this.getProperty("password"),
+   this.getProperty("driver"));
+  me.add(c3p0Plugin);
+ }
+
+ /**
+  * 配置路由
+  */
+ @Override
+ public void configRoute(Routes arg0) {
+  // TODO Auto-generated method stub
+
+
+ }
+
+}
